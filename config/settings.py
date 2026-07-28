dad4544a35e8684f40ad7065c9a9cf66f0ffec0b
usman649 <usman20060623@gmail.com>
2026-07-28 16:42:24 +0500

    fix: ALLOWED_HOSTS and SECRET_KEY fallback

--- config/settings.py
+++ config/settings.py
@@ -26,8 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv("SECRET_KEY")
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-vercel-fallback-key-12345')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
