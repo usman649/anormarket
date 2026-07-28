8b45248e0dff6f0a712be298576ab67281b42a57
usman649 <usman20060623@gmail.com>
2026-07-28 16:34:38 +0500

    feat: updated supabase database pooler connection

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
+
 
 load_dotenv()
 
@@ -81,10 +83,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres.dguxfxyonjlwbxlqhujq:123454321usmanes@aws-0-ap-southeast-2.pooler.supabase.com:6543/postgres',
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
